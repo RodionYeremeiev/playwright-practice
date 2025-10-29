b81acfc4546134e672449a5890487c9672fa6baf
Rod <62612622+RodionYeremeiev@users.noreply.github.com>
2025-10-29 22:22:05 +0200

    test Junit integration with @UsePlaywright (#4)
    Co-authored-by: Rod <ryitlearning@gmail.com>

--- src/test/java/BasicPageTests.java
+++ src/test/java/BasicPageTests.java
@@ -1,19 +1,22 @@
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.junit.UsePlaywright;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class BasicPageTests extends BaseTest{
+@UsePlaywright
+public class BasicPageTests {
 
     @Test
-    void shouldClickButton() {
+    void shouldClickButton(Page page) {
         page.navigate("data:text/html,<script>var result;</script><button onclick='result=\"Clicked\"'>Go</button>");
         page.locator("button").click();
         assertEquals("Clicked", page.evaluate("result"));
     }
 
     @Test
-    void shouldCheckTheBox() {
+    void shouldCheckTheBox(Page page) {
         page.setContent("<input id='checkbox' type='checkbox'></input>");
         page.locator("input").check();
         assertTrue((Boolean) page.evaluate("() => window['checkbox'].checked"));
